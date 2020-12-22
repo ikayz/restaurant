56bb8be537400aefc1569adddca48c62c3c936e0
Isaac Miti <ikayz@live.com>
2020-12-22 18:36:02 +0200

    Add waiter fields

--- src/Waiter.java
+++ src/Waiter.java
@@ -1,3 +1,9 @@
 public class Waiter {
-
+    String name;
+    String dateOfBirth;
+    String position;
+    int age;
+    String residence;
+    String gender = "Unspecified";
+    String startDate;
 }
